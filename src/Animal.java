ccb66900bb240d2928ed721ac039e8340a553fdc
GeorgDeveloper <ifreyd@yandex.ru>
2023-05-27 00:54:05 +0300

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Animal {
+    private String name;
+    private String type;
+    private List<String> commands;
+
+    public Animal(String name, String type) {
+        this.name = name;
+        this.type = type;
+        this.commands = new ArrayList<>();
+    }
+
+    
+}
+
+
+
+
